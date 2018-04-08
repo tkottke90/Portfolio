35ba0c4ecbf66c1c6f203e2bd42acbf596fe9930
tkottke90 <altotom90@gmail.com>
2018-04-08 18:27:51 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,40 @@
+// Frameworks
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
 
+import { AngularFireModule } from 'angularfire2';
 
+// Framework Components
+
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatCardModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatTableModule,
+  MatToolbarModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSlideToggleModule,
+  MatStepperModule,
+  MatProgressSpinnerModule,
+  MatNativeDateModule} from '@angular/material';
+
+// Custom Components
 import { AppComponent } from './app.component';
+
+
 
 
 @NgModule({
@@ -10,7 +42,48 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatStepperModule,
+    MatProgressSpinnerModule,
+    MatNativeDateModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatStepperModule,
+    MatProgressSpinnerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCIgvwOS5FOZTcJixnV7DGraA7Tfc3Jlkw',
+    authDomain: 'op-7f877.firebaseapp.com',
+    databaseURL: 'https://op-7f877.firebaseio.com',
+    projectId: 'op-7f877',
+    storageBucket: 'op-7f877.appspot.com',
+    messagingSenderId: '33209669393'
+  }
 };
