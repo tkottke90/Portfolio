b39fdecdce9a68cd7d81f9c3671830ba18a06a18
tkottke90 <altotom90@gmail.com>
2018-05-03 21:52:46 -0500

    Added Input to Github Event Component

--- src/app/lib/github-event.component/github-event.component.ts
+++ src/app/lib/github-event.component/github-event.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { GithubApiService,
+         GithubEventData,
+         CreateEvent,
+         DeleteEvent,
+         IssuesEvent,
+         PushEvent,
+         WatchEvent } from '../../services/github-api.service';
 
 @Component({
     selector: 'app-github-event',
@@ -6,5 +13,36 @@ import { Component } from '@angular/core';
     styleUrls: [ './github-event.component.scss' ]
 })
 export class GithubEventComponent {
+    date: String;
+    action_icon: String;
+    header: String;
 
+    @Input()
+    event (event: GithubEventData) {
+        this.date = this.genTimelineStr(new Date(event.Date));
+        this.header = event.message;
+    }
+
+    genTimelineStr(date: Date): string {
+        let diff = Date.now() - date.getTime();
+
+        const days = Math.floor( diff / (1000 * 60 * 60 * 24 ));
+        diff -=  days * (1000 * 60 * 60 * 24);
+        const hours = Math.floor( diff / (1000 * 60 * 60 ));
+        diff -= hours * (1000 * 60 * 60);
+        const mins = Math.floor( diff / 1000 * 60 );
+        diff -= mins * (1000 * 60);
+        const secs = Math.floor( diff / 1000 );
+        diff -= secs * (1000);
+
+        if (days > 0) {
+            return `about ${days} days ago`;
+        } else if (hours > 0) {
+            return `about ${hours} hours ago`;
+        } else if (mins > 0) {
+            return `about ${mins} minutes ago`;
+        } else if (secs > 0) {
+            return `about ${secs} seconds ago`;
+        }
+    }
 }
