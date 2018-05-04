137b720eb09123ccceee58ee2767777e32d0fd83
tkottke90 <altotom90@gmail.com>
2018-05-03 20:57:54 -0500

    Added GithubEventComponent

---
+++ src/app/lib/github-event.component/github-event.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-github-event',
+    templateUrl: './github-event.component.html',
+    styleUrls: [ './github-event.component.scss' ]
+})
+export class GithubEventComponent {
+
+}
