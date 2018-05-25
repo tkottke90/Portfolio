51973fa095299dcebf8961607b7070cb3cabc1f5
tkottke90 <altotom90@gmail.com>
2018-05-25 18:29:44 -0500

    Added Project Object

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -7,6 +7,54 @@ export class FirestoreService {
 
     DBROOT = '';
 
-    constructor(public AFstore: AngularFirestore) { }
+    constructor(public AFstore: AngularFirestore) {
+
+
+    }
+
+}
+
+export class Project {
+    id: string;
+    name: string;
+
+    url: {
+        project: string;
+        source: string;
+    };
+
+    images: Map<string, string>;
+
+    otherData: Map<string, string>;
+
+    newProject(
+        name: string,
+    ) {
+        this.name = name;
+    }
+
+    addProjectURL(url: string) {
+        this.url.project = url;
+    }
+
+    addSourceURL(url: string) {
+        this.url.source = url;
+    }
+
+    addImg(name: string, url: string) {
+        this.images.set(name, url);
+    }
+
+    rmvImg(name: string) {
+        return this.images.delete(name);
+    }
+
+    addData(name: string, info: string) {
+        this.otherData.set(name, info);
+    }
+
+    rmvData(name: string) {
+        return this.otherData.delete(name);
+    }
 
 }
