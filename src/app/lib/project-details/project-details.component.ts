3b2f568bee6579d603577e2b3449dab493262ff6
tkottke90 <altotom90@gmail.com>
2018-06-12 13:25:12 -0500

    Removed project details info from work view

--- src/app/lib/project-details/project-details.component.ts
+++ src/app/lib/project-details/project-details.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Project } from '../../services/firestore.service';
 
 @Component({
   selector: 'app-project-details',
   templateUrl: './project-details.component.html',
   styleUrls: ['./project-details.component.scss']
 })
-export class ProjectDetailsComponent implements OnInit {
+export class ProjectDetailsComponent implements OnInit, OnChanges {
+
+  @Input() selectedProject: Project;
+
+  iconKeys = [];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
+    this.iconKeys = Object.keys(this.selectedProject.Icons);
   }
 
 }
