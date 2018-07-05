c5eebadec6a5ab211cda6a3250f7160c278032a8
tkottke90 <altotom90@gmail.com>
2018-07-05 10:30:27 -0500

    <feat> Change about component margins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { ProjectDisplayService } from './services/project.display';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'app';
   overlay: boolean;
 
