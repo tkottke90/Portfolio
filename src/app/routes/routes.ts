b502f0442f76686ed68fe64d86f9c787b4862bf8
tkottke90 <altotom90@gmail.com>
2018-07-06 12:19:28 -0500

    <feat> Update contact route config

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,12 +1,13 @@
 import { RouterModule, Routes} from '@angular/router';
 import { AboutViewComponent } from '../frames/frame-about/about-view.component';
 import { WorkViewComponent } from '../frames/work-view/work-view.component';
+import { ContactViewComponent } from '../frames/contact-view/contact-view.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/about', pathMatch: 'full' },
     { path: 'about', component: AboutViewComponent },
     { path: 'work', component: WorkViewComponent },
-    { path: 'content', component: AboutViewComponent }
+    { path: 'contact', component: ContactViewComponent }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
