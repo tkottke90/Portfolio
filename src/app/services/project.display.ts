070ed529614d78f81abc1c82463e22946474a99b
tkottke90 <altotom90@gmail.com>
2018-07-06 11:22:28 -0500

    <feat> Add router subscription to project display
    Projec display includes variables that manage the toolbar and
    whether or not it is animated.  Only when the about view is visible
    will the toolbar be allowed to animate.  In the Work and Contact views
    the toolbar will be opaque.

--- src/app/services/project.display.ts
+++ src/app/services/project.display.ts
@@ -1,4 +1,5 @@
 import { Injectable, ElementRef } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 import { Project } from './firestore.service';
@@ -10,4 +11,29 @@ export class ProjectDisplayService {
 
     lockToolbar: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
     toolbarTransparent: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+
+    constructor(
+        private router: Router
+    ) {
+        router.events.subscribe(event => {
+            if (event instanceof NavigationStart) {
+                switch (event.url) {
+                    case '/work':
+                    case '/contact':
+                        this.toolbarTransparent.next(false);
+                        this.lockToolbar.next(true);
+                        break;
+                    case '/about':
+                        this.toolbarTransparent.next(true);
+                        this.lockToolbar.next(false);
+                        break;
+                }
+
+                window.scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
+                  });
+            }
+        });
+    }
 }
