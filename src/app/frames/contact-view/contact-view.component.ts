f322b15c71d84839ecc65eea2e3ac95c8def6069
tkottke90 <altotom90@gmail.com>
2018-07-06 22:52:31 -0500

    <feat> Building form controls

--- src/app/frames/contact-view/contact-view.component.ts
+++ src/app/frames/contact-view/contact-view.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from '../../services/firestore.service';
 
+class ConactMessage {
+  constructor(
+    public name: string,
+    public email: string,
+    public phone: string | '',
+    public message: string
+  ) {}
+}
 @Component({
   selector: 'app-contact-view',
   templateUrl: './contact-view.component.html',
@@ -7,9 +16,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactViewComponent implements OnInit {
 
-  constructor() { }
+  model = new ConactMessage('test', 'test@example.com', '123-456-7890', 'This is my message');
+  submitted = false;
+
+  constructor(private fs: FirestoreService) {}
 
   ngOnInit() {
   }
 
+
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  get diagnostic() {
+    return JSON.stringify(this.model);
+  }
 }
