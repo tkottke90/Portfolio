964be733d6e77eed84d66294403e9d58005b96c3
tkottke90 <altotom90@gmail.com>
2018-07-25 14:53:35 -0500

    <feat> Add Tree Logic

--- src/app/lib/treemotif/treemotif.component.ts
+++ src/app/lib/treemotif/treemotif.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+enum Seasons {
+  Spring,
+  Summer,
+  Fall,
+  Winter
+}
 
 @Component({
   selector: 'app-treemotif',
@@ -7,9 +14,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TreemotifComponent implements OnInit {
 
-  constructor() { }
+  sdates = [];
+
+  season: Seasons;
+
+  constructor() {
+    this.season = Seasons.Spring;
+    this.getDates();
+    console.log(this.sdates);
+    this.setSeason();
+  }
 
   ngOnInit() {
+  }
+
+  getDates() {
+    const now = new Date();
+    this.sdates.push(new Date(now.getFullYear(), 3, 20));
+    this.sdates.push(new Date(now.getFullYear(), 6, 21));
+    this.sdates.push(new Date(now.getFullYear(), 9, 22));
+    this.sdates.push(new Date(now.getFullYear(), 12, 21));
+  }
+
+  setSeason() {
+
+    const now = new Date();
+    for (let i = 3; i > 0; i--) {
+      console.log(now.getTime());
+      console.log(this.sdates[i].getTime());
+      if (now.getTime() > this.sdates[i].getTime()) {
+        this.season = i;
+        break;
+      }
+    }
   }
 
 }
